e73035be794f39184e7804443484fdb1be90489e
Phablo Raylan Souza Santos <phablo.santos@castgroup.com.br>
2024-10-23 19:42:51 -0300

    Initial commit

---
+++ src/main/java/com/phabloraylan/beleza/BelezaApplication.java
@@ -0,0 +1,13 @@
+package com.phabloraylan.beleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phabloraylan/beleza/BelezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phabloraylan.beleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
